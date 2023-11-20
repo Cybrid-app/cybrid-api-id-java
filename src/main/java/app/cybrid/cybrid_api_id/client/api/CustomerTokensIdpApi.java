a72f6674fab54c3c8a97f820510b9b0255210656
brent-cybrid <brent@cybrid.app>
2023-11-20 15:56:51 +0000

    v0.110.9

--- src/main/java/app/cybrid/cybrid_api_id/client/api/CustomerTokensIdpApi.java
+++ src/main/java/app/cybrid/cybrid_api_id/client/api/CustomerTokensIdpApi.java
@@ -26,7 +26,7 @@ import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-17T23:05:05.208276Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-11-20T15:51:27.259495Z[Etc/UTC]")
 public class CustomerTokensIdpApi {
     private ApiClient apiClient;
 
